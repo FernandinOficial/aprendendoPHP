523b9d8a0fe94ca917058031de350f88e9dd6cf1
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-05-07 17:22:56 -0300

    loops commit

--- loop.php
+++ loop.php
@@ -1,13 +1,42 @@
 <?php
 //em produção
-    while
 
-    dowhile
+    // $i = 0;
+    // while ($i < 6)
+    // {
+    //     echo $i;
+    //     $i++;
+    // }
+    // echo '<br>';
 
-    for
+    // $i = 1;
+    // do 
+    // {
+    //     echo $i;
+    //     $i++;
+    // }while ($i < 6);
+    
+    // echo '<br>';
 
-    if
+    // $i = 8;
+    // do 
+    // {
+    //     echo $i;
+    //     $i++;
+    // }while ($i < 6);
 
-    foreach
+    echo '<br>';
+
+    $i = 1;
+    // for (expression1, expression2, expression3) {
+    //     // code block
+    // }
+    for ($x = 0; $x <= 10; $x++) {
+        echo "O número é: $x <br>";
+    }
+
+    // if
+
+    // foreach
 
 ?>
