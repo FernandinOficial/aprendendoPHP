a0b618fbed98898e881cc548f2462bc62da234d8
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-05-14 15:03:36 -0300

    tutorial Commit

---
+++ tutorial.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial</title>
+</head>
+
+<body>
+
+    <!-- Parte 1 -->
+    <!--
+    <?php
+
+
+    // PRINT com <BR>
+    // echo("ola mundo <br>");
+    // print("olamundo");
+    
+
+    // phpinfo();
+    
+
+    // VARIAVEIS
+    // $teste01 = "Só um teste";
+    // echo $teste01;
+    
+
+    // TIPOS DE COMENTARIOS PHP
+    /*
+        Comentario
+        de varias
+        linhas
+    */
+    // comentario de uma linha so
+    
+
+    // HTML no PHP
+    // print('<span style=color:#00FFFF;>Texto</span>');
+    // print('<span style=color:red;>Texto</span>');
+    echo ("ola");
+
+    ?>--> <!--ENCERRAMENTO PARTE 1-->
+
+
+
+    <!-- Parte 2 -->
+    <!---->
+    <?php
+
+
+    // TRATANDO DATAS
+    echo ("ola");
+    ?> <!----> <!--ENCERRAMENTO PARTE 2-->
+
+</body>
+
+</html>
