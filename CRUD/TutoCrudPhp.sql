02960155edb3357bc4fcd0e296b287ba9ad0ca6f
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-05-17 17:21:01 -0300

    commit

--- CRUD/TutoCrudPhp.sql
+++ CRUD/TutoCrudPhp.sql
@@ -1,11 +1,91 @@
--- create database TutoCrudPhp;
-use TutoCrudPhp;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 17/05/2024 às 22:16
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
 
--- create TABLE Cliente (
---     Id INT PRIMARY KEY AUTO_INCREMENT,
---     Nome VARCHAR(60) NOT NULL,
---     Email VARCHAR(150) NOT NULL,
---     Cidade VARCHAR(100),
---     UF VARCHAR(2)
--- )
-select * from Cliente;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tutocrudphp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `Id` int(11) NOT NULL,
+  `Nome` varchar(60) NOT NULL,
+  `Email` varchar(150) NOT NULL,
+  `Cidade` varchar(100) DEFAULT NULL,
+  `UF` varchar(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cliente`
+--
+
+INSERT INTO `cliente` (`Id`, `Nome`, `Email`, `Cidade`, `UF`) VALUES
+(1, 'fernando', 'fernando@gmail.com', 'Itapira', ''),
+(2, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(3, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(4, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(5, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(6, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(7, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(8, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(9, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(10, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(11, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(12, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(13, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(14, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(15, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(16, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(17, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(18, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(19, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(20, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(21, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(22, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(23, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp'),
+(24, 'fernando', 'fernando@gmail.com', 'Itapira', 'sp');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ CRUD/TutoCrudPhpQUERY.sql
@@ -0,0 +1,11 @@
+-- create database TutoCrudPhp;
+use TutoCrudPhp;
+
+-- create TABLE Cliente (
+--     Id INT PRIMARY KEY AUTO_INCREMENT,
+--     Nome VARCHAR(60) NOT NULL,
+--     Email VARCHAR(150) NOT NULL,
+--     Cidade VARCHAR(100),
+--     UF VARCHAR(2)
+-- )
+select * from Cliente;
