8394d41149801196c0d1da5083a9a44c90c28a48
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-05-27 16:34:54 -0300

    commit

---
+++ segurancaPHP/includes/db_connect.php
@@ -0,0 +1,4 @@
+<?php 
+    include_once 'psl-config.php';  // Já que functions.php não está incluso
+    $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+?>
--- segurancaPHP/includes/functions.php
+++ segurancaPHP/includes/functions.php
@@ -9,6 +9,36 @@
 
         $session = SECURE;  //impedir que JavaScript acesse a identificação da sessão
         $httponly = true;   //força a sessão a usar os cookies
-    }
 
+        if(ini_set('session.use_only_cookies',1) === FALSE)
+        {
+            header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
+            exit();
+        }  
+
+        //parametros de cookies atualizados
+        $cookieParams = session_get_cookie_params();
+        session_set_cookie_params($cookieParams["lifetime"],
+            $cookieParams["path"],
+            $cookieParams["domain"],
+            $secure,
+            $httponly
+        );
+        session_name($session_name);
+        session_start(); //inicia a sessão
+        session_regenerate_id();  //recupera a sessão anterior e deleta a anterior
+    };
+
+    function login($email, $password, $mysqli)
+    {   //Usando definições pré-estabelecidas significa que a injeção de SQL (um tipo de ataque) não é possível. 
+        if($stmt = $mysqli -> prepare("SELECT id, username, password, salt  
+            FROM members
+            WHERE email = ?
+            LIMIT 1")) //vai preparar uma query 
+            {
+                $stmt -> bind_param('s',$email) //vai relacionar $email como parametro
+                $stmt -> execute(); //executa a tarefa estabelecida
+                $stmt -> store_result();
+            }
+    }
 ?>
