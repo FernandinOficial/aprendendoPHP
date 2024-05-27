2728fef2f35016ef73ea41a6e17ca1e6a510946f
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-05-27 14:57:16 -0300

    commit

---
+++ segurancaPHP/includes/psl-config.php
@@ -0,0 +1,15 @@
+<!-- configurações globais -->
+<!-- Página de Conexão de Banco de Dados -->
+
+<?php 
+
+    define("HOST","localhost");  //host que quer se conectar
+    define("USER", "sec_user"); //usuario para conexao
+    define("PASSWORD", "4Fa98xkHVd2XmnfK"); //senha do usuario
+    define("DATABASE","secure_login");  //qual database
+
+    define("CAN_REGISTER","any");    //quem pode registrar, qualquer um
+    define("DEFAULT_ROLE","member");   
+
+    define("SECURE",FALSE) //SOMENTE PARA DESENVOLVIMENTO!!!!
+?>
--- segurancaPHP/segurancaQUERY.sql
+++ segurancaPHP/segurancaQUERY.sql
@@ -2,11 +2,36 @@
 
 USE secure_login;
 
-CREATE TABLE members 
-(    
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,    
-    username VARCHAR(30) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    password CHAR(128) NOT NULL,
-    salt CHAR(128) NOT NULL
-);
+-- criar um usuario de segurança
+-- CREATE USER 'sec_user'@'localhost' IDENTIFIED BY 'eKcGZr59zAa2BEWU';
+-- GRANT SELECT, INSERT, UPDATE ON `secure_login`.* TO 'sec_user'@'localhost'; -- permissoes como select, insert, update
+
+-- tabela no MySQL com o nome de "membros".
+-- CREATE TABLE members 
+-- (    
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,    
+--     username VARCHAR(30) NOT NULL,
+--     email VARCHAR(50) NOT NULL,
+--     password CHAR(128) NOT NULL,
+--     salt CHAR(128) NOT NULL
+-- );
+
+-- tabela para registrar as tentativas de login.
+-- CREATE TABLE login_attempts
+-- (   
+--     user_id INT(11) NOT NULL,    
+--     time VARCHAR(30) NOT NULL
+-- );
+
+-- login
+-- INSERT INTO members VALUES
+-- (
+--     1, 
+--     'test_user', 
+--     'test@example.com',
+--     '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc',
+--     'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef'
+-- );
+
+-- SELECT * FROM members;
+
