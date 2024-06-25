a05bf5b7a9385dd46bfa88953bb8c380dde26ecd
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-06-25 16:05:08 -0300

    commit

--- PHPbootstrap/users.php
+++ PHPbootstrap/users.php
@@ -3,13 +3,6 @@ require_once 'connect.php';
 require_once 'header.php';
 
 echo "<div class='container'>";
-if (isset($_POST['delete'])) 
-{
-    $sql = "DELETE FROM users WHERE user_id = " . $_POST['userid'];
-    if ($con->query($sql) === TRUE) {
-        echo "<div class='alert alert-success'>Successfully deleted user</div>";
-    }
-}
 
 $sql = "SELECT * FROM users";
 $result = $con->query($sql);
@@ -36,7 +29,7 @@ if ($result->num_rows > 0) {
             echo "<td>" . $row['address'] . "</td>";
             echo "<td>" . $row['contact'] . "</td>";
             echo "<td><input type='submit' name='delete' value='Delete' class='btn btn-danger' /></td>";
-            echo "<td><a href='edit.php?id=" . $row['user_id'] ."' class='btn btn-info'>Edit</a></td>";
+            echo "<td><a href='edit.php?id=" . $row['user_id'] . "' class='btn btn-info'>Edit</a></td>";
             echo "</tr>";
             echo "</form>";
         }
